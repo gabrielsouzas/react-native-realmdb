6f61d62290fbb33d4295758a7f97b3910cd91689
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2024-03-12 17:42:59 -0300

    add: Install dependencies

---
+++ src/database/realm.ts
@@ -0,0 +1,9 @@
+import Realm from 'realm';
+import { OrderSchema } from './Schemas/OrderSchema';
+
+export const getRealm = async () =>
+  await Realm.open({
+    path: 'decode-app',
+    schema: [OrderSchema],
+    schemaVersion: 3,
+  });
